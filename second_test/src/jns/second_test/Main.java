5beab92493d92541febf8cb4aaa32c5c0dcef15e
joonas <leipuri.junnuinen@gmail.com>
2019-06-27 15:00:14 +0300

    Adding to previous just to be sure

--- second_test/src/jns/second_test/Main.java
+++ second_test/src/jns/second_test/Main.java
@@ -8,17 +8,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	
 	EventManager eventManager;
+	boolean gameRunning;
 	
 	@Override
 	public void onEnable() {
-		// TODO Auto-generated method stub
-		getLogger().info("Joonas mod is enabled v1.1.1");
+		// Informs the server console that the plugin is active
+		// and sets gameRunning to false
+		getLogger().info("Joonas plugin is enabled v1.1.1");
+		gameRunning = false;
 		super.onEnable();
 	}
 	
 	@Override
 	public void onDisable() {
-		// TODO Auto-generated method stub
 		super.onDisable();
 	}
 	
@@ -26,6 +28,7 @@ public class Main extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("hello") && sender instanceof Player) {
 			getLogger().info("Command worked, " + sender.getName() + ", with argument size: " + Integer.toString(args.length));
+			
 			if(args.length >= 1) {
 				if(args[0].equalsIgnoreCase("run")) {
 					getLogger().info("Here you go: " + args[0]);
